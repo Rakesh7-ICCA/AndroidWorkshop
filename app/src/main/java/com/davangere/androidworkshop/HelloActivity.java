b0bab179c4cbfeca9a7fd08bd9b7b39cf393983e
Rakesh7-ICCA <gaurakesh42@gmail.com>
2026-09-01 11:52:32 +0530

    Click listner

--- app/src/main/java/com/davangere/androidworkshop/HelloActivity.java
+++ app/src/main/java/com/davangere/androidworkshop/HelloActivity.java
@@ -1,6 +1,9 @@
 package com.davangere.androidworkshop;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,16 @@ public class HelloActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView tv = findViewById(R.id.caller);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Welcome to FDP");
+            }
+        });
+
     }
 }
